2d749fad87adf7705f23953cc3d1f5da57d87a77
Sten Nierop <1100080@hr.nl>
2025-02-19 11:29:52 +0100

    Fleshed out the Player class, the player can receive money and/or experience and added a GetPlayerStats() method for easy printing out current player stats.

--- Player.cs
+++ Player.cs
@@ -2,9 +2,45 @@ class Player
 {
     public string Name;
     public int CurrentHitPoints;
-    public int MaximumHitPoints;
     public Weapon CurrentWeapon;
     public Location CurrentLocation;
 
-    // Code underneath.
+    public int Money;
+    public int Experience;
+
+    // Player class constructor;
+    public Player(string name, int currentHitPoints = 100, Location location)
+    {
+        this.Name = name;
+        this.CurrentHitPoints = currentHitPoints;
+        this.CurrentWeapon = null;
+        this.CurrentLocation = location;
+        this.Money = 0;
+        this.Experience = 0;
+
+    }
+
+    // Returning simple player stats;
+    public string GetPlayerStats() => $"| Name: {this.Name}; Health: {this.CurrentHitPoints}; Money: {this.Money}; EXP: {this.Experience} |";
+
+    // Receiving rewards for finishing quests;
+    public string ReceiveQuestRewards(int money = 0, int experience = 0)
+    {
+        this.Money += money;
+        this.Experience += experience;
+
+        if (money == 0)
+        {
+            return $"Hooray! You received {experience} experience!";
+        }
+
+        if (experience == 0)
+        {
+            return $"Hooray! You received {money} coin(s)!";
+        }
+
+        return $"Hooray! You received {money} coin(s), and {experience} experience!";
+
+    }
+   
 }
