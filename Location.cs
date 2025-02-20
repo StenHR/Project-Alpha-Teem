bc58dbda731e61faf39a84997e52d600464ccf5a
lars7b <larszevenbergen97@gmail.com>
2025-02-20 11:23:27 +0100

    Update Location.cs

--- Location.cs
+++ Location.cs
@@ -1,14 +1,14 @@
 public class Location
 {
-    public int ID
-    public string Name
-    public string Description
-    public Quest QuestAvailableHere
-    public Monster MonsterLivingHere
-    public Location LocationToNorth
-    public Location LocationToSouth
-    public Location LocationToEast
-    public Location LocationToWest
+    public int ID;
+    public string Name;
+    public string Description;
+    public Quest QuestAvailableHere;
+    public Monster MonsterLivingHere;
+    public Location LocationToNorth;
+    public Location LocationToSouth;
+    public Location LocationToEast;
+    public Location LocationToWest;
 
     public Location(int id, string name, string description, 
                 Quest questAvailableHere = null, Monster monsterLivingHere = null)
