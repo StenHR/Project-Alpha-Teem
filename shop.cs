a4e5fbe04a8f2099480d5978b30d85caadd418b7
lars7b <larszevenbergen97@gmail.com>
2025-03-06 12:15:09 +0100

    Shop class
    static shop class

---
+++ shop.cs
@@ -0,0 +1,13 @@
+﻿public class Quest
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public Quest(int id, string name, string description)
+    {
+        ID = id;
+        Name = name;
+        Description = description;
+    }
+}
