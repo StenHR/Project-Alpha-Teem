fc4bb725ab9968b988f7f8788cab909bb732ae84
Kai <kaitholen@outlook.com>
2025-03-11 10:32:24 +0100

    Added farmers field quest and battle.cs updates

--- locations/FarmHouse/ClearFarmersFieldQuest.cs
+++ locations/FarmHouse/ClearFarmersFieldQuest.cs
@@ -2,16 +2,19 @@
 
 public class ClearFarmersFieldQuest : QuestLine
 {
-    public List<Monster> snakes;
+    public List<Monster> snakes = [];
 
     public override void QuestLogic()
     {
         // Implementeer hier de quest logica, dit kunnen trackers, acties, dialogen, puzzels, etc. zijn.
         // Als een quest verwacht dat je naar een andere area gaat en daar iets doet, maak dan een nieuwe questline aan voor die area met de verwachte acties.
         Print.Dialog("Snakes have been attacking farmers in the field. To clear the quest kill 3 snakes");
+        Monster temp = World.MonsterByID(2);
+
         for (int i = 0; i < 3; i++)
         {
-            snakes.Add(World.MonsterByID(2));
+            Monster snake = new Monster(temp.ID, temp.Name, temp.BaseDamage, temp.CurrentHitPoints, temp.critChance);
+            snakes.Add(snake);
         }
 
         Player player = World.Player;
@@ -19,11 +22,16 @@ public class ClearFarmersFieldQuest : QuestLine
         foreach (Monster snake in snakes)
         {
             Battle battle = new Battle(player, snake);
-            battle.Turn()
+            while (battle.Active)
+            {
+                battle.Turn();
+            }
             Thread.Sleep(2000);
-            Console.Clear()
+            Console.Clear();
         }
-        Print.Dialog("You have cleared the Alchemist's Garden of all the spiders. The people of the town are grateful for your help.");
+        Print.Dialog("You have cleared the Farmers Field of all the snakes. The farmers of the town are grateful for your help.",
+            style: Print.PrintStyle.TypeEffect);
+        World.Player.ReceiveQuestRewards(100, 50);
         Thread.Sleep(1000);
     }
 }
